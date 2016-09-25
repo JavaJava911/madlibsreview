0d64e3855b97c434587152ca45211aed51fefe42
Chad Martin <cmartin584@students.mchenry.edu>
2016-09-25 07:27:09 -0500

    Add files via upload

---
+++ EnterpriseFool.py
@@ -0,0 +1,165 @@
+#Variables
+
+
+name1 = raw_input("Who is your favorite hero (first, last, or nickname only)? ")
+
+name2 = raw_input("Who would be a good adversary for your hero (first, last or nickname name only)? ")
+
+name3 = raw_input("a Woman's name or nickname: ")
+
+name4 = raw_input("What are harmless, yet annoying creatures? ")
+
+adj1 = raw_input("adjective (person) 1: ")
+
+adj2 = raw_input("adjective (size) 2: ")
+
+adj3 = raw_input("another adjective (beaches): ")
+
+candy4 = raw_input("What is your favorite ice cream flavor? ")
+
+verb1 = raw_input("verb1 (how you get from place to place (I_______) ")
+
+verb2 = raw_input("A traditional dance: ")
+
+cartoon = raw_input("A Classic Cartoon Character (name only): ")
+
+choice1 = raw_input("What was your 1 favorite toy as a child? ")
+
+choice2 = raw_input("noun2, your favorite beverage? ")
+
+choice3 = raw_input("Noun3, on a deserted Island, you can't live without this? ")
+
+rank = raw_input("Military rank? (private, Admiral) ")
+
+time = raw_input("What's a unique time of day (second breakfast)? ")
+
+place = raw_input("Noun, a large place, _____: "+"\n")
+
+
+# end of variables
+print ("Original Music")
+print("Soundtrack of")
+print("Little Bunny Foo-Foo"+"\n")
+
+print ("Poem of the great and fabulous "+name1+"\n")
+
+
+print("Little "+rank+" "+name1)
+
+print(verb1+" around "+ place)
+
+print("Scoopin' up "+candy4+" "+name2+"s")
+
+print("And "+verb2+"ing"+" on their "+adj3+" heads""\n")
+
+print("Down came the most benevolent "+name3+ " and she said:""\n")
+
+print("Now! little "+rank+" "+name1)
+
+print("I don't want to see you")
+
+print("Scoopin' up "+candy4+" "+name2+"s")
+
+print("And "+verb2+"ing"+ " on their "+adj3+" heads"+"\n")
+
+
+print("I'll give you 3 "+choice1)
+
+print("But, if you don't "+cartoon+" right now!")
+
+print("I'll turn your friends and their friends into "+ adj1+" "+name4+"\n")
+
+
+
+print("After the very next " +time+"\n")
+
+#V2
+
+print("Little "+rank+" "+name1)
+
+print(verb1+" round the "+place)
+
+print("Scoopin' up "+candy4+" "+name2+"s")
+
+print("And "+verb2+"ing"+" on their "+adj3 + " heads""\n")
+
+
+print("Down came the most benevolent "+name3+ " and she said:""\n")
+
+
+print("Now! little "+rank+" "+name1)
+
+print("I don't want to see you")
+
+print("Scoopin' up " +candy4+" "+name2+"s")
+
+print("And "+verb2+"ing"+ " on their "+adj3+" "+"heads""\n")
+
+print("I'll give you 2 "+choice2)
+
+print("But, if you don't "+ cartoon+" right now!")
+
+print("I'll turn your friends and their friends into "+adj1+" "+name4+"\n")
+
+
+print("After the very next "+time+"\n")
+
+#V3
+print("Little "+rank+" "+name1)
+print(verb1+"ing"+ "round the "+place)
+
+print("Scoopin' up "+candy4+" "+name2+"s")
+print("And "+verb2+"ing"+ " on their "+adj3 +" heads,""\n")
+
+print("Down came the most benevolent "+name3+ " and she said:""\n")
+
+print("Now! little "+rank+" "+name1)
+
+print("I don't want to see you")
+
+print("Scoopin' up the " +candy4+" "+name2+"s")
+
+print("And "+verb2+"ing"+ "on their "+adj3+" heads""\n")
+
+
+print("I'll give you just 1 more, "+choice1+" but,")
+
+print("Don't be a fool, if you refuse to "+cartoon+" right now!")
+
+print("I'll turn your friends and their friends into "+adj1+" "+name4+"\n")
+
+
+print("After the very next "+time+"\n")
+
+#V4
+print("Little "+rank+" "+name1)
+print(verb1+"ing"+" round the "+place)
+
+print("Scoopin' up "+candy4+" "+name2+"s")
+print("And "+verb2+"ing"+ " on their "+adj3+" heads,""\n")
+
+print("Down came the most benevolent "+name3+" and she said:""\n")
+
+print("Now! little "+rank+" "+name1)
+
+print("I didn't want to see you")
+
+print("Scoopin' up " +candy4+" "+name2+"s")
+
+print("And "+verb2+"ing"+ " on their "+adj3+" "+"heads"+"\n")
+
+print("I gave you 3 "+choice1+ " 2 "+choice2+ " and 1 super duper "+ choice3)
+
+print("You just couldn't resist those "+adj3+ " " +candy4+" "+name2+" So,")
+
+print("True to my word "+rank+ " POOF!!!, your friends and their friends,")
+print("are all "+adj1+" "+name4)
+
+print ("and the moral of the story is? "+"\n")
+
+print ("no good can come from "+candy4+" "+name2+ " and "+ adj1+ " "+name4)
+
+print("trying to watch Star trek while "+verb1+"ing" " up their "+cartoon+"s""")
+
+
+
